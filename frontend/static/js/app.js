ba8286ea9c50a676043e5d07f7e5200dc5580367
confidence <confidencenkeonye@gmail.com>
2025-12-01 15:24:42 +0100

    Updated app.js file to fetch data from the api

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,3 +1,42 @@
-fetch('/api/tasks/')
-.then(res => res.json())
-.then(data => console.log(data));
+document.addEventListener('DOMContentLoaded', loadTasks);
+
+function loadTasks() {
+    fetch('/api/tasks/')
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById('taskList');
+            list.innerHTML = '';
+
+            data.forEach(task => {
+                let item = document.createElement('div');
+                item.className = 'task-item';
+                item.innerHTML = `
+                    <h3> ${task.title} </h3>
+                    <p> ${task.content} </p>
+                `;
+                list.appendChild(item);
+            });
+
+        });
+}
+
+function createTasl() {
+    const title = document.getElementById('taskTitle').value;
+    const content = document.getElementById('taskContent').value;
+
+    fetch('api/tasks/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            content: content
+        })
+    })
+    .then(() =>{
+        loadTasks();
+        document.getElementById('taskTitle').value = '';
+        document.getElementById('taskContent').value = '';
+    })
+}
