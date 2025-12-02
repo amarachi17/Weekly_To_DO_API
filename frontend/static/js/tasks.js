19def8adcd087cec9211e99e57d3c7690370e188
confidence <confidencenkeonye@gmail.com>
2025-12-02 10:34:55 +0100

    Created tasks.js file to load task

---
+++ frontend/static/js/tasks.js
@@ -0,0 +1,39 @@
+async function loadTasks() {
+    const response = await fetch(API + "tasks/", {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+
+    const data = await response.json();
+
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    data.forEach(task => {
+        list.innerHTML += `
+            <div class="bg-white p-4 shadow rounded mb-2">
+                <h3 class="font-bold">${task.title}</h3>
+                <p>${task.description}</p>
+                <p class="text-sm text-gray-500">Category: ${task.category}</p>
+            </div>
+        `;
+    });
+}
+
+async function createTask() {
+    const title = taskTitle.value;
+    const description = taskDesc.value;
+    const category = taskCategory.value;
+
+    await fetch(API + "tasks/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ title, description, category })
+    });
+
+    loadTasks();
+}
+
+loadTasks();
