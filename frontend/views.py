43e12884087d5499432a622d5c3f50cb04d514fb
confidence <confidencenkeonye@gmail.com>
2025-12-02 11:13:28 +0100

    Created views for each page

--- frontend/views.py
+++ frontend/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'frontend/ind.html')
+    return render(request, 'frontend/base.html')
+
+def login_page(request): 
+    return render(request, 'frontend/login.html')
+
+def register_page(request): 
+    return render(request, 'frontend/register.html')
+
+def dashboard_page(request): 
+    return render(request, 'frontend/dashboard.html')
+
+def categories_page(request): 
+    return render(request, 'frontend/categories.html')
+
+def tasks_page(request): 
+    return render(request, 'frontend/tasks.html')
+
+def reminders_page(request): 
+    return render(request, 'frontend/reminders.html')
