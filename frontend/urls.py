84a57e83e07c7e11c26f4ba2141ec29403a45179
confidence <confidencenkeonye@gmail.com>
2025-12-02 10:04:36 +0100

    Added path for login, regidter

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views
+from django.shortcuts import render
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path("login/", lambda request: render(request, "frontend/login.html"), name="login-page"),
+    path("register/", lambda request: render(request, "frontend/register.html"), name="register-page"),
+    path("dashboard/", lambda request: render(request, "frontend/dashboard.html"), name="dashboard"),
+
+    path("categories/", lambda request: render(request, "frontend/categories.html"), name="categories"),
+    path("tasks/", lambda request: render(request, "frontend/tasks.html"), name="tasks"),
+    path("reminders/", lambda request: render(request, "frontend/reminders.html"), name="reminders"),
+
 ]
