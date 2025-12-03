85f99dae281d9b71b107146116d9f61b13284097
confidence <confidencenkeonye@gmail.com>
2025-12-03 08:34:16 +0100

    Created models for user manager

--- api/models.py
+++ api/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 import uuid
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 # from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(email, password, **extra_fields)
+
 
 # Creating a custom user model
 class User(AbstractUser):
@@ -16,9 +35,17 @@ class User(AbstractUser):
     update_at = models.DateTimeField(auto_now=True)
 
     username = None
+    email = models.EmailField(unique=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    
     # REQUIRED_FIELDS = ['email']
     def save(self, *args, **kwargs):
         # Ensure password is made hashed before saving to project password from been used
